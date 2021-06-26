d6a00ba60d02ee7e2e16bd205868a7fc229d7a64
swapnilwani008 <86426172+swapnilwani008@users.noreply.github.com>
2021-06-26 17:06:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header"
+import Footer from "./footer"
+import Info from "./info"
+
+function App(){
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+
+  );
+}
+export default App;
---
+++ info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return(
+     <div className = "note" >
+        <h1> shape ai </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis non dolorem vel reiciendis corporis, quaerat, eveniet excepturi quia incidunt, ut placeat officiis exercitationem asperiores inventore nobis maiores minima hic. Harum?</p>
+        
+     </div>
+    );
+}
+export default Info;
